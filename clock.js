296ad0da45d0d4f8333574dd9686ecc546c3e97a
c-roos <chrisroos1313@gmail.com>
2019-11-25 16:54:35 -0600

    Preprocessed JSX

--- clock.js
+++ clock.js
@@ -1,22 +1,51 @@
-const testVar = 'Hello'
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-class MyClass extends React.Component {
-    render() {
-        return (
-            <div>
-                <h1>Test Component</h1>
-            </div>
-        );
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var testVar = 'Hello';
+
+var MyClass = function (_React$Component) {
+    _inherits(MyClass, _React$Component);
+
+    function MyClass() {
+        _classCallCheck(this, MyClass);
+
+        return _possibleConstructorReturn(this, (MyClass.__proto__ || Object.getPrototypeOf(MyClass)).apply(this, arguments));
     }
-}
 
-const testElement = (
-    <div id='test'>
-        <p>Hello</p>
-    </div>
+    _createClass(MyClass, [{
+        key: 'render',
+        value: function render() {
+            return React.createElement(
+                'div',
+                null,
+                React.createElement(
+                    'h1',
+                    null,
+                    'Test Component'
+                )
+            );
+        }
+    }]);
+
+    return MyClass;
+}(React.Component);
+
+var testElement = React.createElement(
+    'div',
+    { id: 'test' },
+    React.createElement(
+        'p',
+        null,
+        'Hello'
+    )
 );
 
 function initClocks() {
     ReactDOM.render(testElement, document.getElementById('container'));
-    ReactDOM.render(<MyClass />, document.getElementById('test'));
+    ReactDOM.render(React.createElement(MyClass, null), document.getElementById('test'));
 }
