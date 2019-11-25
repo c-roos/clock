2e4d57cabd1956b606da2cd67905815cbad0ca12
c-roos <chrisroos1313@gmail.com>
2019-11-25 14:36:30 -0600

    Testing a component

--- clock.js
+++ clock.js
@@ -1,7 +1,18 @@
+class MyClass extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Test Component</h1>
+            <div>
+        );
+    }
+}
+
 const testElement = (
-    <div>
+    <div id='test'>
         <p>Hello</p>
     </div>
 );
 
 ReactDOM.render(testElement, document.getElementById('container'));
+ReactDOM.render(<MyClass />, document.getElementById('test'));
